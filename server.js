17b47eaa4fbe282f81bce7a290dac9005b5befea
Melissa Kepler <mmkepler@yahoo.com>
2017-04-06 14:19:44 +0000

    fixed js and html

--- server.js
+++ server.js
@@ -1,40 +1,40 @@
-
 var express = require("express");
+//moment.js to deal with time conversions and checks
 var moment = require("moment");
 var path = require("path");
 var app = express();
 
+//expess serves the html
+app.use(express.static('views'));
 
-app.use(express.static(path.resolve(__dirname, 'views')));
-
-
-/*app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname + '/index.html'));
-});*/
-
-
-/*app.get("/dateinfo/:dateInput", function(req, res){
-    var data = req.params.dateInput;
+app.get("/:dateInput", function(req, res){
+    var unixDate, naturalDate, temp;
+    var input =  req.params.dateInput;
     
-    
-    var unixDate;
-    var naturalDate;
-    
-    if(Number.isNAN(data)){
-        naturalDate = moment(data).format("MMMM D, YYYY");
-        unixDate = new Date(data).getTime()/1000;
-        if(!naturalDate.isValid()){
-            naturalDate = null;
-        }
+    //if input is a number, then it can be a unix timestamp
+    if(Number(input)){
+        unixDate = Number(input);
+        naturalDate = moment.unix(input).format("MMMM DD, YYYY");
     } else {
-        unixDate = data;
-        naturalDate = moment.unix(data).format("MMMM D, YYYY");
-        
+        /*attempts to put non number entry into date format,
+        now I can check it using moment.js's isValid*/
+        temp = moment(new Date(input));
+        //checks for validity
+        if(moment(input,"MMMM DD, YYYY").isValid()) {
+            naturalDate = moment(temp).format("MMMM DD, YYYY");
+            unixDate = moment(temp).unix();
+        } else {
+            //if meets none of the above conditions
+            naturalDate = null;
+            unixDate = null;
+        }
     }
-    //res.json({"unix": + unixDate, "natural": naturalDate});
-});*/
+    
+    //using res.json caused a MIME type issue in chrome.
+   res.send(JSON.stringify({"unix": unixDate, "natural": naturalDate}));
+});
 
-
+//listen at the port for Heroku, or the port for c9.io if unavailable.
 app.listen(process.env.PORT || 8080, function(err){
     if(err){
         throw err;
